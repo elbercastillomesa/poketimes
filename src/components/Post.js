9e01711dbb064f4c41f06dc245681d8bde592601
Elber Castillo Mesa <elber.castillo@correounivalle.edu.co>
2019-05-20 14:05:21 -0500

    setup a function to Dispatch actions to the reducer, then create a button and a funtion to call function, in this case Delete function. The calling is show in console when the button is clicked.

--- src/components/Post.js
+++ src/components/Post.js
@@ -3,12 +3,21 @@ import { connect } from 'react-redux';
 
 class Post extends Component {
 
+    handleClick = () => {
+        this.props.deletePost(this.props.post.id)
+    }
+
     render(){
 
         const post = this.props.post ? (
             <div className="post">
                 <h4 className="center">{this.props.post.title}</h4>
                 <p>{this.props.post.body}</p>
+                <div className="center">
+                    <button className="btn grey" onClick={this.handleClick}>
+                        Delete Post
+                    </button>
+                </div>
             </div>
         ) : (
             <div className="center"> Loading post...</div>
@@ -39,5 +48,17 @@ const mapStateToProps = (state, ownProps) => {
     }*/
 }
 
+const mapDistpatchToProps = (dispatch) =>{
+    return {
+        deletePost: (id) => { 
+            dispatch(
+                {
+                    type: 'DELETE_POST',
+                    id: id
+                }
+            )
+        }
+    }
+}
 
-export default connect(mapStateToProps)(Post);
+export default connect(mapStateToProps, mapDistpatchToProps)(Post);
